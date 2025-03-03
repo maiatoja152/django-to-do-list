cb57bde19275332404280edaa90f05a0b93160c9
Jacob Maiato <jacob@jacobmaiato.com>
2025-03-03 17:41:44 -0500

    Create the Task model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    completed = models.BooleanField(
+        help_text="Whether the task has been completed.",
+        db_default=False,
+    )
+    title = models.TextField(
+        help_text="The title of the task. (e.g. \"Do laundry\")",
+    )
+    description = models.TextField(
+        help_text="A longer description of the task.",
+        blank=True,
+    )
+    due_date = models.DateTimeField(
+        help_text="The date and time at which the task is due for completion.",
+        null=True,
+    )
+
+    def __str__(self):
+        return str(self.title)
