66a7d3e12690756478ee0ca164e7693c320ebeb8
Jacob Maiato <jacob@jacobmaiato.com>
2025-03-04 14:15:59 -0500

    Create Task List view

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+from django.http import HttpResponse
+
+from typing import List
+
+from .models import Task
+
+from bs4 import BeautifulSoup
+from bs4 import Tag
+
+
+class ViewTests(TestCase):
+    """Base class for testing views."""
+    view_name: str
+
+
+    def get_view_response(self) -> HttpResponse:
+        """Get an HttpResponse for the view."""
+        return self.client.get(reverse(self.view_name))
+
+
+class TaskListViewTests(ViewTests):
+    """Tests for the Task List view."""
+    view_name = "todo:task-list"
+    
+
+    def test_no_tasks(self) -> None:
+        """
+        Test that an appropriate message is displayed when no tasks exist.
+        """
+        response: HttpResponse = self.get_view_response()
+        self.assertContains(response, "You have no tasks.")
+        self.assertQuerySetEqual(response.context["task_list"], [])
+    
+
+    def test_multiple_tasks(self) -> None:
+        """
+        Test the view when multiple tasks exist. The view should display the
+        tasks' titles and checkboxes which are checked only for tasks that are
+        completed.
+        """
+        tasks: List[Task] = []
+        for i in range(5):
+            tasks.append(Task.objects.create(title=f"Task {i}"))
+        
+        response: HttpResponse = self.get_view_response()
+        self.assertQuerySetEqual(
+            response.context["task_list"],
+            tasks,
+            ordered=False,
+        )
+        soup: BeautifulSoup = BeautifulSoup(response.content)
+        for task in tasks:
+            self.assertContains(response, task.title)
+            # Test checkbox
+            checkbox: Tag = soup.select(f"input#task-{task.id}-checkbox")[0]
+            if task.completed:
+                self.assertIn("checked", checkbox.attrs)
+            else:
+                self.assertNotIn("checked", checkbox.attrs)
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpRequest
 
-# Create your views here.
+from .models import Task
+
+
+def task_list(request: HttpRequest) -> HttpResponse:
+    return render(request, "todo/task-list.html", {"task_list": Task.objects.all()})
